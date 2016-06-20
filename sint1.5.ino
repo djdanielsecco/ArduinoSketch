9d0611ae981df48d3d35d73f3b8e25a891318bb4
djdanielsecco <djdanielsecco@gmail.com>
2016-06-19 21:34:37 -0300

    Projeto

---
+++ sint1.5.ino
@@ -0,0 +1,45 @@
+
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+byte pitch;
+
+void handleNoteOn(byte channel, byte pitch, byte velocity)
+{
+
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity)
+{
+
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+//byte channel;
+
+//byte velocity;
+
+  MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
+
+  // Do the same for NoteOffs
+  MIDI.setHandleNoteOff(handleNoteOff);
+
+  // Initiate MIDI communications, listen to all channels
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(115200);
+   MIDI.read();
+  byte (MIDI.getData1()) == pitch;
+}
+
+void loop() {
+  MIDI.read();
+  {
+    
+    Serial.println(pitch);
+  delay(0.01);}
+
+  }
